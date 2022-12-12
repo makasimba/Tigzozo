1af3ebaf2c22e2e22046afe74a87ffe3dda329e3
makasimba <simbarashe.makahamadze@gmail.com>
2022-12-12 15:51:37 +0200

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+let icon = document.getElementById('theme-icon');
+icon.onclick = function () { document.body.classList.toggle("dark-theme");}
+
---
+++ scripts/style.js
@@ -0,0 +1,2 @@
+// $('.banner .container .col-md-2 img:even').css("padding-top", "4em");
+// $('.gram .container .col-md-2 img:odd').css("padding-top", "4em");
